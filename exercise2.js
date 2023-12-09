7652529a84b7d21ccfb363ee5fc4c670b39c455a
Shakeem <100867480+SusanoSix@users.noreply.github.com>
2023-12-09 09:16:02 -0500

    Initial updload
    //sends two prompts requesting first and last name, then returns 
    //the full name as an alert function using the fullName var

---
+++ exercise2.js
@@ -0,0 +1,29 @@
+
+// Evaluate what answers you would get if you ran this in the
+// Javascript Console in Google Chrome. Answer the questions then 
+// check them by copying them and running it in the console yourself 
+// line by line 
+
+
+// add variable "firstName" and "lastName" // so that they equal your name
+
+// create a variable that holds the answer // of "firstName" + " " + "lastName"
+
+var firstName = prompt("what is your first name?");
+var lastName = prompt("what is your last name?");
+var fullName = firstName + " " + lastName;
+alert("Hellow, " + fullName);
+
+//the above code sends two prompts requesting first and last name, then returns 
+//the full name as an alert function using the fullName var
+
+
+// Evaluate this question. What is a + b?
+var a = 34;
+var b = 21;
+a = 2;
+a + b // what is the answer here? the answer is 23 as a was redefined later
+
+// What is c equal to? c is undefined
+var c;
+
